3dd1b34c9b51188b2f970814f34ec6044dc99cb3
Jeffrey Triandi Sabarman <jeffsabarman@Jeffreys-MacBook-Pro.local>
2022-07-13 22:06:27 +0700

    add embed youtube and responsive

--- components/Home/YoutubeChanel.tsx
+++ components/Home/YoutubeChanel.tsx
@@ -1,8 +1,29 @@
+import { Grid } from '@mui/material';
 import React from 'react';
 import HeaderLayout from '../HeaderLayout';
+import { useResponsive } from 'helpers/custom-hooks';
 
 const YoutubeChanel = () => {
-  return <HeaderLayout title="Subscribe Channel Youtube Kami"></HeaderLayout>;
+  /** Media Query */
+  const { Phone, SmallDesktop } = useResponsive();
+
+  return (
+    <HeaderLayout title="Subscribe Channel Youtube Kami">
+      <Grid item sx={{ mt: Phone ? 0 : SmallDesktop ? 4 : 8 }}>
+        <iframe
+          src="https://www.youtube.com/embed/kGexUZIDsQI"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{
+            width: '90vw',
+            height: Phone ? '50vw' : '45vw',
+          }}
+        />
+      </Grid>
+    </HeaderLayout>
+  );
 };
 
 export default YoutubeChanel;
