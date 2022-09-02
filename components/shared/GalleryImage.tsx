b4ffb2b3a79ab4c2bd900bde6499aee6461f684f
Jeffrey Triandi Sabarman <jeffsabarman@Jeffreys-MacBook-Pro.local>
2022-09-02 22:15:14 +0700

    handle carousel image gallery facilites

---
+++ components/shared/GalleryImage.tsx
@@ -0,0 +1,52 @@
+import { useTheme, Box } from '@mui/material';
+//@ts-ignore
+import ImageGallery from 'react-image-gallery';
+
+interface GalleryImageProps {
+  images:
+    | {
+        original: string;
+        thumbnail?: string;
+        originalAlt?: string;
+        description?: string;
+      }[]
+    | undefined;
+  slideDuration?: number;
+  autoPlay?: boolean;
+  thumbnailPosition?: 'top' | 'right' | 'bottom' | 'left';
+  showFullscreenButton?: boolean;
+  showPlayButton?: boolean;
+  showBullets?: boolean;
+}
+
+function GalleryImage({
+  images,
+  slideDuration = 600,
+  autoPlay = false,
+  thumbnailPosition = 'bottom',
+  showFullscreenButton = false,
+  showPlayButton = false,
+  showBullets = false,
+}: GalleryImageProps) {
+  const theme = useTheme();
+
+  return (
+    <Box
+      style={{
+        backgroundColor: theme.palette.primary.main,
+      }}
+    >
+      <ImageGallery
+        items={images}
+        slideDuration={slideDuration}
+        autoPlay={autoPlay}
+        thumbnailPosition={thumbnailPosition}
+        showFullscreenButton={showFullscreenButton}
+        showPlayButton={showPlayButton}
+        showBullets={showBullets}
+      />
+    </Box>
+  );
+}
+
+export default GalleryImage;
