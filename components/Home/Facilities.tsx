4ee69eb44becb26b1878d8b3a4a6a631654f99e0
martinsuhendra <suhendra.martin90@gmail.com>
2022-09-12 21:06:41 +0700

    fix: facilities home components

--- components/Home/Facilities.tsx
+++ components/Home/Facilities.tsx
@@ -1,11 +1,14 @@
 import styled from '@emotion/styled';
 import { Box, BoxProps, Grid, useTheme, useMediaQuery } from '@mui/material';
-import React, { CSSProperties, FC, ReactNode } from 'react';
+import React, { CSSProperties, FC, ReactNode, useState } from 'react';
 import HeaderLayout from '../HeaderLayout';
 import { useResponsive } from 'helpers/custom-hooks';
 import { SliderCarousel } from '../Carousel';
 import Pool from '@/public/images/swimming-pool.jpg';
 import GalleryImage from '../shared/GalleryImage';
+import FacilityImage from '../Facilities/FacilityImage';
+import FacilitiesSlider from '../Facilities/FacilitiesSlider';
+import { FACILITIES } from 'helpers/constants';
 
 const images = [
   {
@@ -37,6 +40,18 @@ const images = [
 const Facilities = () => {
   const theme = useTheme();
   const { Phone, Tablet, SmallDesktop } = useResponsive();
+  const [focusImageKey, setFocusImageKey] = useState('');
+  const [showSlider, setShowSlider] = useState(false);
+
+  /** Media Queries */
+  const largerThanPhone = useMediaQuery(theme.breakpoints.up('md'));
+  const customLargerPhone = useMediaQuery('(max-width:800px)');
+
+  /** Function */
+  const handleZoomImage = (key: string) => {
+    setShowSlider(true);
+    setFocusImageKey(key);
+  };
 
   return (
     <Box bgcolor={theme.palette.primary.main}>
@@ -46,17 +61,29 @@ const Facilities = () => {
           title="Fasilitas"
           subtitle="We are a school that empowers students to thrive in academic excellence; personal, social and emotional growth that are needed for success in their live and livelihood."
         >
-          <Box maxWidth={Tablet ? '100%' : SmallDesktop ? '75vw' : '60vw'}>
-            <GalleryImage
-              autoPlay
-              slideDuration={800}
-              showFullscreenButton
-              showPlayButton
-              {...{ images }}
-            />
-          </Box>
+          <Grid mt={1} container spacing={3}>
+            {FACILITIES?.map((fac, index) => (
+              <Grid
+                xs={customLargerPhone ? 12 : SmallDesktop ? 6 : 4}
+                item
+                key={index}
+                mb={customLargerPhone ? 2 : 0}
+              >
+                <FacilityImage
+                  handleClick={() => handleZoomImage(fac.key)}
+                  title={fac.title}
+                  image={fac.image}
+                />
+              </Grid>
+            ))}
+          </Grid>
         </HeaderLayout>
       </Grid>
+      <FacilitiesSlider
+        open={showSlider}
+        handleClose={() => setShowSlider(false)}
+        focusImage={focusImageKey}
+      />
     </Box>
   );
 };
